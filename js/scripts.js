28ad5e650c5e06ce604d923d8dea11c5e30e322a
Bappaditya <721135bappa@gmail.com>
2022-11-02 10:41:51 +0530

    updated js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,27 @@
 'use strict';
+const divLog_in=document.querySelector('.log-in');
+const divSign_up=document.querySelector('.sign-up');
+const overlay=document.querySelector('.overlay');
+
 document.querySelector('.login-btn').addEventListener('click',function(){
-    document.querySelector('.log-in').classList.remove("hidden");
-    document.querySelector('.overlay').classList.remove("hidden");
+    divLog_in.classList.remove("hidden");
+    overlay.classList.remove("hidden");
 });
 document.querySelector('.signup-btn').addEventListener('click',function(){
-    document.querySelector('.sign-up').classList.remove("hidden");
-    document.querySelector('.overlay').classList.remove("hidden");
+    divSign_up.classList.remove("hidden");
+    overlay.classList.remove("hidden");
     });
 document.querySelector('.login-cross').addEventListener('click',function(){
-    document.querySelector('.log-in').classList.add("hidden");
-    document.querySelector('.overlay').classList.add("hidden");
+    divLog_in.classList.add("hidden");
+    overlay.classList.add("hidden");
 })
 document.querySelector('.signup-cross').addEventListener('click',function(){
     console.log('clicked');
-    document.querySelector('.sign-up').classList.add("hidden");
-    document.querySelector('.overlay').classList.add("hidden");
+    divSign_up.classList.add("hidden");
+    overlay.classList.add("hidden");
+})
+document.querySelector('.overlay').addEventListener('click',function(){
+    divSign_up.classList.add("hidden");
+    overlay.classList.add("hidden");
+    divLog_in.classList.add("hidden");
 })
